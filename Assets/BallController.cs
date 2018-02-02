5ca773dcef5b20fcfa94a39c8148e4f428cfbf32
kento.Hirabayashi <aspirationkent@gmail.com>
2018-02-03 00:03:49 +0900

    課題提出　課題：得点を表示しよう
    小さい星と雲に当たった場合は10点、大きい星と雲に当たった場合は20点加算されるようにしています。

---
+++ Assets/BallController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+	private GameObject scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+		this.scoreText = GameObject.Find ("ScoreText");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+
+
+
+		}
+
+	
+
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "SmallStarTag" || other.gameObject.tag == "SmallCloudTag")
+			this.score += 10;
+		this.scoreText.GetComponent<Text> ().text = "Score " + this.score + "pt";
+
+		
+		if (other.gameObject.tag == "LargeStarTag" || other.gameObject.tag == "LargeCloudTag")
+			this.score += 20;
+		this.scoreText.GetComponent<Text> ().text = "Score " + this.score + "pt";
+
+
+	}
+
+}
